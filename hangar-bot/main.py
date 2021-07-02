4f06230091548efaa994529da996154ed237d09f
WoutersThijs <r0793167@student.thomasmore.be>
2021-07-02 12:28:36 +0200

    Telegram news listening

--- hangar-bot/main.py
+++ hangar-bot/main.py
@@ -1,3 +1,4 @@
+import keep_alive
 import discord
 import os
 import secrets
@@ -14,14 +15,17 @@ tClient.start()
 async def on_ready():
     print("Logged in as {0.user}".format(dClient))
 
-    @tClient.on(events.NewMessage(chats="@hangarbottest"))
+    @tClient.on(events.NewMessage(chats="@the_block_crypto"))
     async def news_handler_1(event):
-       await dClient.get_channel(844201849602572288).send(":newspaper:   The Block's News Feed    :newspaper:\n \n" + event.text)
+       await dClient.get_channel(845251997795614730).send(":newspaper:   The Block's News Feed    :newspaper:\n \n" + event.text)
+
+    @tClient.on(events.NewMessage(chats="@oneminuteletter"))
+    async def news_handler_2(event):
+       await dClient.get_channel(845251997795614730).send(":newspaper:   One Minute Letter    :newspaper:\n \n" + event.text)
 
     @tClient.on(events.NewMessage(chats="@hangarbottest"))
-    async def news_handler_2(event):
-       await dClient.get_channel(844201849602572288).send(":newspaper:   One Minute Letter    :newspaper:\n \n" + event.text)
-
+    async def news_handler_3(event):
+       await dClient.get_channel(845251997795614730).send(":newspaper:   Test message   :newspaper:\n \n" + event.text)
 
 @dClient.event
 async def on_message(message):
@@ -34,5 +38,5 @@ async def on_message(message):
     if message.content.startswith('/hangar add tradinghours'):
         await message.channel.send('Ready to send trading hours in this channel!')
 
-
+keep_alive.keep_alive()
 dClient.run(secrets.disc_client)
